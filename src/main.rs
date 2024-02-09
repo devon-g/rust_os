8dc574b3047832b1f22fa8926360abdfb8c25de2
Devon Gardner <dgardner365@gmail.com>
2024-02-09 02:31:48 -0500

    A minimal rust kernel

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
-#![no_std]
-#![no_main]
+#![no_std] // Don't link rust std as it is os dependent
+#![no_main] // Disable rust entry points because they depend on c libs
 
 use core::panic::PanicInfo;
 
-// Custom entrypoint that doesn't link to c libraries
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+static HELLO: &[u8] = b"Hello Madam President Madelyn Khoury!";
+
+// Custom entrypoint that doesn't require c libraries
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xd;
+        }
+    }
+
     loop {}
 }
